c5df58efa913a1be71407ec4b38339f128d2342e
Christopher Amadra Titus <kris.skyfallgraphix@gmail.com>
2023-08-11 22:48:25 +0300

    change

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+setup (
+        name='math_py',
+        version='0.1.0',
+        license='Apache License 2.0',
+        author='Christopher Amadra Titus',
+        description='A simple math calculator package',
+        long_description='README.md',
+        author_email='groc.coders.schools@gmail.com',
+        url='https://groc-coders.blogspot.com',
+        download_url='https://groc-coders.blogspot.com/downloads.html',
+        packages=['math_py'],
+        zip_safe=False
+    )
